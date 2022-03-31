b6c5395d3334d346b3cbcee662908a58a321c9b4
allenhu12 <allenhu12@qq.com>
2022-03-31 22:49:43 +0800

    add the nvim config

---
+++ nvim/init.vim
@@ -0,0 +1,16 @@
+set rtp +=~/.vim
+call plug#begin('~/.vim/nvim/plugged')
+Plug 'tomasiser/vim-code-dark'
+Plug 'beanworks/vim-phpfmt'
+Plug 'sbdchd/neoformat'
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+Plug 'nvim-telescope/telescope-file-browser.nvim'
+call plug#end()
+
+
+
+
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath=&runtimepath
+source ~/.vim/vimrc
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,33 @@
+local packer = require("packer")
+packer.startup(
+  function(use)
+   -- Packer 可以管理自己本身
+   use 'wbthomason/packer.nvim'
+   -- 你的插件列表...
+   -- colorscheme
+   use("folke/tokyonight.nvim")
+   -- others
+   use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })
+   use({ "akinsho/bufferline.nvim", requires = { "kyazdani42/nvim-web-devicons", "moll/vim-bbye" }})
+   use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons" } })
+   use("arkav/lualine-lsp-progress")
+   use { 'nvim-telescope/telescope.nvim', requires = { "nvim-lua/plenary.nvim" } }
+   -- telescope extensions
+   use "LinArcX/telescope-env.nvim"
+   use("glepnir/dashboard-nvim")
+   -- project
+   use("ahmedkhalf/project.nvim")
+   -- treesitter （新增）
+    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+end)
+
+-- 每次保存 plugins.lua 自动安装插件
+pcall(
+  vim.cmd,
+  [[
+    augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    augroup end
+  ]]
+)
