fec6078570e439b47503abf6424181f5bf6279a1
allenhu12 <allenhu12@qq.com>
2022-03-31 22:49:43 +0800

    add the nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,16 @@
+-- basic configuration
+require('basic')
+-- keybindings configuration
+require('keybindings')
+-- plugins configuration
+require('plugins')
+-- colorscheme configuration
+require("colorscheme")
+-- plugin configuration
+require("plugin-config.nvim-tree")
+require("plugin-config.bufferline")
+require("plugin-config.lualine")
+require("plugin-config.telescope")
+require("plugin-config.dashboard")
+require("plugin-config.project")
+require("plugin-config.nvim-treesitter")
