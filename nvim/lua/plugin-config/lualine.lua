66d59c6e64e117a818ab5421cfa4309eb016a437
allenhu12 <allenhu12@qq.com>
2022-08-29 10:29:43 +0800

    neovim configurations update

--- nvim/lua/plugin-config/lualine.lua
+++ nvim/lua/plugin-config/lualine.lua
@@ -14,6 +14,13 @@ lualine.setup({
   },
   extensions = { "nvim-tree", "toggleterm" },
   sections = {
+    lualine_a = {
+    {
+      'filename',
+      file_status = true, -- displays file status (readonly status, modified status)
+      path = 2 -- 0 = just filename, 1 = relative path, 2 = absolute path
+    }
+  },
     lualine_c = {
       "filename",
       {
