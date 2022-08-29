f89d44d4fdb091dbf018111cd2a8ecad9e8bac33
allenhu12 <allenhu12@qq.com>
2022-08-29 21:03:45 +0800

    modify nvim with macbook pro

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -1,5 +1,7 @@
-vim.g.mapleader = ","
-vim.g.maplocalleader = ","
+-- vim.g.mapleader = ","
+-- vim.g.maplocalleader = ","
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 local map = vim.api.nvim_set_keymap
 -- 复用 opt 参数
 local opt = {noremap = true, silent = true }
