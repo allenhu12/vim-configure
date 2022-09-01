ddf93c46d22e314c38e0ac353a8eeaa0c0849707
allenhu12 <allenhu12@qq.com>
2022-09-01 17:10:49 +0800

    add the utility folder for some useful script

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -111,4 +111,25 @@ map("n", "<C-\\>", ":Telescope buffers<CR>", opt)
 map("n", "f/", ":Telescope live_grep<CR>", opt)
 -- easymotion
 map("n", "<C-f>", "<Plug>(easymotion-overwin-f)", opt)
+-- trouble
+-- Lua
+
+map("n","<C-o>", ":Troublel<cr>",opt)
+
+vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>",
+  {silent = true, noremap = true}
+)
+vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>",
+  {silent = true, noremap = true}
+)
+
 return pluginKeys
