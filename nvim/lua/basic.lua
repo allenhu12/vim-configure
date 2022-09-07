0ef18695eac1d768f1c2053b8024e960238a612a
allenhu12 <allenhu12@qq.com>
2022-09-07 18:12:11 +0800

    add the keybindings for command line mode

--- nvim/lua/basic.lua
+++ nvim/lua/basic.lua
@@ -87,24 +87,47 @@ vim.g.ctrlsf_auto_preview = 1
 vim.g.ctrlsf_populate_qflist = 1
 vim.opt.wrap = true
 vim.cmd "set whichwrap+=<,>,[,],h,l"
+
+--- Coc Settings ---
 vim.cmd([[
+let g:coc_global_extensions = ['coc-json', 'coc-vimlsp']
 " Use tab for trigger completion with characters ahead and navigate.
 " NOTE: There's always complete item selected by default, you may want to enable
 " no select by `"suggest.noselect": true` in your configuration file.
 " NOTE: Use command ':verbose imap <tab>' to make sure tab is not mapped by
 " other plugin before putting this into your config.
-inoremap <silent><expr> <TAB>
+ " inoremap <silent><expr> <TAB>
+ "       \ coc#pum#visible() ? coc#pum#next(1) :
+ "       \ CheckBackspace() ? "\<Tab>" :
+ "       \ coc#refresh()
+ " inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
+
+inoremap <silent><expr> <C-j>
       \ coc#pum#visible() ? coc#pum#next(1) :
       \ CheckBackspace() ? "\<Tab>" :
       \ coc#refresh()
-inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
+inoremap <expr><C-k> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
+
+function! CheckBackspace() abort
+  let col = col('.') - 1
+  return !col || getline('.')[col - 1]  =~# '\s'
+endfunction
+
+" Use <TAB> to trigger completion.
+if has('nvim')
+  inoremap <silent><expr> <C-o> coc#refresh()
+else
+  inoremap <silent><expr> <c-@> coc#refresh()
+endif
 
 " Make <CR> to accept selected completion item or notify coc.nvim to format
 " <C-g>u breaks current undo, please make your own choice.
 inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
                               \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+
 ]]
 )
+--- Coc Settings end ---
 
 local function is_git_repo()
     local f = io.popen("git rev-parse --show-toplevel")
@@ -127,3 +150,4 @@ function! ToggleQuickFix()
     endif
 endfunction
 ]], false)
+
